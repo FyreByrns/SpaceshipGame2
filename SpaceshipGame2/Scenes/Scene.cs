a78f154d2269e8f696aad7d272627eae42866495
Gavin White <horace5413@gmail.com>
2021-04-13 12:02:34 -0700

    namespace scenes

---
+++ SpaceshipGame2/Scenes/Scene.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceshipGame2.Scenes {
+	abstract class Scene {
+		public SpaceGame owner;
+		public abstract void Update(float elapsed);
+
+		public Scene(SpaceGame owner) {
+			this.owner = owner;
+		}
+	}
+}
+
--- SpaceshipGame2/SpaceGame.cs
+++ SpaceshipGame2/SpaceGame.cs
@@ -1,4 +1,5 @@
 ﻿using PixelEngine;
+using SpaceshipGame2.Scenes;
 using System.Runtime.InteropServices;
 
 namespace SpaceshipGame2 {
@@ -26,33 +27,6 @@ namespace SpaceshipGame2 {
 
 		[DllImport("kernel32.dll")]
 		static extern void FreeConsole();
-	}
-
-	class GameScene : Scene {
-		public World.World world;
-
-		public override void Update(float elapsed) {
-			if (owner.GetKey(Key.W).Down) world.cameraPosition_w.y--;
-			if (owner.GetKey(Key.S).Down) world.cameraPosition_w.y++;
-			if (owner.GetKey(Key.A).Down) world.cameraPosition_w.x--;
-			if (owner.GetKey(Key.D).Down) world.cameraPosition_w.x++;
-
-			world.Update(owner, elapsed);
-			world.Draw(owner);
-		}
-
-		public GameScene(SpaceGame owner) : base(owner) {
-			world = new World.World((0, 0), (owner.ScreenWidth, owner.ScreenHeight));
-		}
-	}
-
-	abstract class Scene {
-		public SpaceGame owner;
-		public abstract void Update(float elapsed);
-
-		public Scene(SpaceGame owner) {
-			this.owner = owner;
-		}
 	}
 }
 
