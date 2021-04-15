1733b08f0e9d312260baeed5b2bbc1201940e8f2
Gavin White <horace5413@gmail.com>
2021-04-15 12:55:21 -0700

    tweak default hovered colour

--- SpaceshipGame2/UI/UIColourCollection.cs
+++ SpaceshipGame2/UI/UIColourCollection.cs
@@ -6,7 +6,7 @@ namespace SpaceshipGame2.UI {
 
 		public static readonly UIColourCollection DefaultUp = new UIColourCollection { outline = new Pixel(200, 200, 200), fill = new Pixel(100, 100, 100), foreground = new Pixel(255, 255, 255) };
 		public static readonly UIColourCollection DefaultDown = new UIColourCollection { outline = DefaultUp.fill, fill = DefaultUp.outline, foreground = DefaultUp.foreground };
-		public static readonly UIColourCollection DefaultHover = new UIColourCollection { outline = new Pixel(220, 220, 220), fill = new Pixel(120, 120, 120), foreground = DefaultUp.foreground };
+		public static readonly UIColourCollection DefaultHover = new UIColourCollection { outline = new Pixel(220, 220, 220), fill = new Pixel(200, 200, 200), foreground = DefaultUp.fill };
 		public static readonly UIColourCollection DefaultDisabled = new UIColourCollection { outline = new Pixel(100, 100, 100), fill = new Pixel(10, 10, 10), foreground = new Pixel(200, 200, 200) };
 	}
 }
